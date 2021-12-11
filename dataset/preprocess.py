b12c00c4e6358e17a4457e6fd70d5da49fa1a0a5
ssuncheol <52492949+ssuncheol@users.noreply.github.com>
2021-12-11 17:40:50 +0900

    Update preprocess.py

--- dataset/preprocess.py
+++ dataset/preprocess.py
@@ -1,12 +1,17 @@
 import pandas as pd 
+import json
+import os
+import re
+
+
+# label  {'chung' : 0, 'junla' :1, 'kyung' :2, 'zeju' : 3}
 
 def preprocess(path):
     file_list = os.listdir(path)
 
     file_list = [file for file in file_list if file.endswith('json')]
     file_list
-
-
+    
     li = []
     li2 = []
     for i in file_list :
@@ -28,22 +33,30 @@ def preprocess(path):
 
     filter_li = []
     for i in df2.index : 
-        if len(set(df['standard_form'][i].split(' ')) - set(df['dialect_form'][i].split(' '))) > 2 : 
+        if len(set(df['standard_form'][i].split(' ')) - set(df['dialect_form'][i].split(' '))) > 1 : 
             filter_li.append(i)
                 
     df2 = df2.loc[filter_li]
     df2 = df2.reset_index(drop=True)
+    
+    hangul = re.compile('[^ ㄱ-ㅣ가-힣+]')
+    df2['standard_form'] = df2['standard_form'].apply(lambda x : hangul.sub('',x))
+    df2['dialect_form'] = df2['dialect_form'].apply(lambda x : hangul.sub('',x))
 
+    special = re.compile(r'[^ A-Za-z0-9가-힣+]')
+    df2['standard_form'] = df2['standard_form'].apply(lambda x : special.sub('',x))
+    df2['dialect_form'] = df2['dialect_form'].apply(lambda x : special.sub('',x))
 
-    tr = df2.loc[:df2.shape[0]*0.8]
-    val = df2.loc[df2.shape[0]*0.8:]
+    tr = train = df2.iloc[5000:]
+    val = df2.iloc[0:2500]
+    test = df2.iloc[2500:5000]
 
-    return tr, val
+    return tr, val, test
 
 
 path = ' '
 
-tr, val = preprocess(path)    
+tr, val, test = preprocess(path)    
 
 tr.to_csv('train.csv')
 val.to_csv('val.csv')
